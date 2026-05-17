35f0c3df414bbe43546bf41494c31c069026b1c9
Cout0 <a113253@uminho.pt>
2026-05-17 18:26:32 +0100

    fix: executeFile

--- Fase3/executeFile.c
+++ Fase3/executeFile.c
@@ -91,123 +91,89 @@ BARALHO aplicarBaralhos(RegrasBaralhos rb) {
     return b;
 }
 
-void aplicarJogo(RegrasJogo rj) {
-    if (rj == NULL || rj->comando == NULL) {
-        printf("ERRO: Regra JOGO não definida!\n");
-        return;
+int encontrarTotalTab(RegrasInit ri) {
+    while (ri != NULL) {
+        if (strcmp(ri->tipoDePilha, "TAB") == 0) return ri->numeroDeCartas;
+        ri = ri->prox;
     }
-    printf("1. [JOGO] %s\n", rj->jogoNome);
+    return 0;
+}
+
+void alocarTabuleiro(ESTADO *jogo, int total) {
+    if (total <= 0) return;
+    int max_inf = 5;
+    jogo->num_pilhas = (total + max_inf - 1) / max_inf;
+    jogo->max_cartas_por_pilha = max_inf + 10;
+    jogo->paciencia = malloc(jogo->num_pilhas * sizeof(CARTA *));
+    for (int i = 0; i < jogo->num_pilhas; i++) {
+        jogo->paciencia[i] = calloc(jogo->max_cartas_por_pilha, sizeof(CARTA));
+        if (!jogo->paciencia[i]) exit(EXIT_FAILURE);
+    }
+}
+
+void preencherTAB(RegrasInit ri, ESTADO *j, BARALHO d, int *idx) {
+    int max_inf = 5;
+    for (int i = 0; i < ri->numeroDeCartas; i++) {
+        int col = i / max_inf, lin = i % max_inf;
+        if (col < j->num_pilhas && lin < j->max_cartas_por_pilha) {
+            j->paciencia[col][lin] = d[(*idx)++];
+        } else (*idx)++;
+    }
+}
+
+void processarNoInit(RegrasInit ri, ESTADO *j, BARALHO d, int *idx) {
+    if (strcmp(ri->tipoDePilha, "TAB") == 0) {
+        preencherTAB(ri, j, d, idx);
+    } else if (strcmp(ri->tipoDePilha, "DESCARTE") == 0) {
+        if (ri->numeroDeCartas > 0) j->fundacao = d[(*idx)++];
+    } else if (strcmp(ri->tipoDePilha, "STOCK") == 0) {
+        j->posicao_topo_do_baralho = *idx;
+        j->cartas_no_baralho = ri->numeroDeCartas;
+    }
+}
+
+void aplicarInit(RegrasInit ri, ESTADO *jogo, BARALHO deck) {
+    if (ri == NULL) return;
+    printf("4. [INIT] Inicializando pilhas:\n");
+    alocarTabuleiro(jogo, encontrarTotalTab(ri));
+    int carta_idx = 0;
+    while (ri != NULL) {
+        processarNoInit(ri, jogo, deck, &carta_idx);
+        ri = ri->prox;
+    }
+}
+
+void aplicarJogo(RegrasJogo rj) {
+    if (rj && rj->comando) printf("1. [JOGO] %s\n", rj->jogoNome);
 }
 
 void aplicarTipo(RegrasTipo rt) {
-    if (rt == NULL) {
-        printf("   [TIPO] Nenhum tipo definido\n");
-        return;
+    if (rt) {
+        printf("3. [TIPO] Tipos de pilhas:\n");
+        imprimirTipos(rt);
     }
-    printf("3. [TIPO] Tipos de pilhas:\n");
-    imprimirTipos(rt);
-}
-
-/**
- * Versão genérica da golf_init que recebe as regras lidas
- */
-void aplicarInit(RegrasInit ri, ESTADO *jogo, BARALHO deck) {
-    if (ri == NULL) {
-        printf("   [INIT] Nenhuma inicialização definida\n");
-        return;
-    }
-    printf("4. [INIT] Inicializando pilhas:\n");
-
-    int total_cartas_tab = 0;
-    int max_cartas_inferred = 5; // Valor base para jogos tipo Golf/Simon (pode ser ajustado)
-    int num_pilhas_inferred = 0;
-
-    // 1ª Passagem: Determinar dimensões necessárias para o tabuleiro (TAB)
-    RegrasInit aux_dim = ri;
-    while (aux_dim != NULL) {
-        if (strcmp(aux_dim->tipoDePilha, "TAB") == 0) {
-            total_cartas_tab = aux_dim->numeroDeCartas;
-            num_pilhas_inferred = (total_cartas_tab + max_cartas_inferred - 1) / max_cartas_inferred;
-            break; 
-        }
-        aux_dim = aux_dim->prox;
-    }
-
-    // Alocação dinâmica do tabuleiro (paciencia)
-    if (total_cartas_tab > 0 && num_pilhas_inferred > 0) {
-        jogo->num_pilhas = num_pilhas_inferred;
-        jogo->max_cartas_por_pilha = max_cartas_inferred + 10; // Margem para movimentos
-
-        jogo->paciencia = (CARTA **)malloc(jogo->num_pilhas * sizeof(CARTA *));
-        if (jogo->paciencia == NULL) {
-            perror("Erro ao alocar memória para as pilhas do jogo");
-            exit(EXIT_FAILURE);
-        }
-        for (int i = 0; i < jogo->num_pilhas; i++) {
-            jogo->paciencia[i] = (CARTA *)calloc(jogo->max_cartas_por_pilha, sizeof(CARTA));
-            if (jogo->paciencia[i] == NULL) {
-                perror("Erro ao alocar memória para cartas da pilha");
-                for (int j = 0; j < i; j++) {
-                    free(jogo->paciencia[j]);
-                }
-                free(jogo->paciencia);
-                exit(EXIT_FAILURE);
-            }
-        }
-    } else {
-        jogo->num_pilhas = 0;
-        jogo->max_cartas_por_pilha = 0;
-        jogo->paciencia = NULL;
-    }
-
-    // 2ª Passagem: Distribuir cartas de acordo com as regras lidas
-    int carta_atual = 0;
-    RegrasInit aux = ri;
-    while (aux != NULL) {
-        if (strcmp(aux->tipoDePilha, "TAB") == 0) {
-            for (int i = 0; i < aux->numeroDeCartas; i++) {
-                int col = i / max_cartas_inferred;
-                int lin = i % max_cartas_inferred;
-                
-                if (col < jogo->num_pilhas && lin < jogo->max_cartas_por_pilha) {
-                    jogo->paciencia[col][lin] = deck[carta_atual++];
-                } else {
-                    carta_atual++; 
-                }
-            }
-        } 
-        // Distribuição para a Fundação/Descarte (DESCARTE)
-        else if (strcmp(aux->tipoDePilha, "DESCARTE") == 0) {
-            if (aux->numeroDeCartas > 0) {
-                jogo->fundacao = deck[carta_atual++];
-            }
-        }
-        // O resto das cartas fica no Stock
-        else if (strcmp(aux->tipoDePilha, "STOCK") == 0) {
-            jogo->posicao_topo_do_baralho = carta_atual;
-            jogo->cartas_no_baralho = aux->numeroDeCartas;
-        }
-        aux = aux->prox;
-    }
-    imprimirInits(ri);
 }
 
 void aplicarMovAuto(RegrasMovAuto rma) {
-    if (rma == NULL) {
-        printf("   [MOV/AUTO] Nenhum movimento definido\n");
-        return;
+    if (rma) {
+        printf("5. [MOV/AUTO] Movimentos automáticos:\n");
+        imprimirMovs(rma);
     }
-    printf("5. [MOV/AUTO] Movimentos automáticos:\n");
-    imprimirMovs(rma);
 }
 
 void aplicarWin(RegrasWin rw) {
-    if (rw == NULL) {
-        printf("   [WIN] Nenhuma condição de vitória definida\n");
-        return;
+    if (rw) {
+        printf("6. [WIN] Condições de vitória:\n");
+        imprimirWins(rw);
     }
-    printf("6. [WIN] Condições de vitória:\n");
-    imprimirWins(rw);
+}
+
+void limparEstado(ESTADO *jogo) {
+    if (jogo->paciencia) {
+        for (int i = 0; i < jogo->num_pilhas; i++) free(jogo->paciencia[i]);
+        free(jogo->paciencia);
+    }
+    if (jogo->B) free(jogo->B);
 }
 
 // ========== FUNÇÃO PRINCIPAL DE EXECUÇÃO ==========
@@ -216,32 +182,15 @@ void execute(RegrasMovAuto rma, RegrasJogo rj, RegrasBaralhos rb,
              RegrasTipo rt, RegrasInit ri, RegrasWin rw) {
 
     ESTADO jogo;
-    memset(&jogo, 0, sizeof(ESTADO)); // Limpa o estado
+    memset(&jogo, 0, sizeof(ESTADO));
 
     printf("\n========== APLICANDO REGRAS DO JOGO ==========\n\n");
     aplicarJogo(rj);
-    printf("\n");
-    
-    BARALHO b = aplicarBaralhos(rb);
-    jogo.B = b;
-
-    printf("\n");
+    jogo.B = aplicarBaralhos(rb);
     aplicarTipo(rt);
-    printf("\n");
-    aplicarInit(ri, &jogo, b);
-    
-    printf("\n");
+    aplicarInit(ri, &jogo, jogo.B);
     aplicarMovAuto(rma);
-    printf("\n");
     aplicarWin(rw);
-    printf("\n");
     printf("========== REGRAS APLICADAS ==========\n\n");
-
-    // Limpeza da memória dinâmica do jogo
-    if (jogo.paciencia != NULL) {
-        for (int i = 0; i < jogo.num_pilhas; i++) {
-            free(jogo.paciencia[i]);
-        }
-        free(jogo.paciencia);
-    }
+    limparEstado(&jogo);
 }
