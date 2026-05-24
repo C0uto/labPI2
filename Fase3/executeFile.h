c2c66ead448abd42700020ee8775577ceb3bf423
C0uto <n.miguel2001@gmail.com>
2026-05-24 18:25:01 +0100

    ...

--- Fase3/executeFile.h
+++ Fase3/executeFile.h
@@ -1,11 +1,8 @@
-#include "interface.h"
-
-void tratarAjuda(void);
-void tratarMover(char *buf, ESTADO *j, RegrasMovAuto rma, RegrasWin rw);
-void tratarUndo(ESTADO *j);
-int acaoSistema(char c, ESTADO *j, RegrasInit ri, const char *n);
-int executarComando(char *buf, ESTADO *j, RegrasMovAuto rma, RegrasInit ri, RegrasBaralhos rb, RegrasWin rw, const char *nome);
-void loopComandos(ESTADO *j, RegrasMovAuto rma, RegrasInit ri, RegrasBaralhos rb, RegrasWin rw, const char *nome);
-void prepararAmbiente(ESTADO *j, RegrasBaralhos rb, RegrasMovAuto rma, RegrasWin rw);
-void execute(RegrasMovAuto rma, RegrasJogo rj, RegrasBaralhos rb,
-             RegrasTipo rt, RegrasInit ri, RegrasWin rw, int carregar_save);
+void tratarAjuda(void); // Necessita de ESTADO, RegrasMovAuto, RegrasWin, RegrasInit, RegrasBaralhos, RegrasTipo
+void tratarMover(char *buf, ESTADO *j, RegrasMovAuto rma, RegrasWin rw); // Necessita de ESTADO, RegrasMovAuto, RegrasWin, RegrasTipo
+void tratarUndo(ESTADO *j); // Necessita de ESTADO
+int acaoSistema(char c, ESTADO *j, RegrasInit ri, const char *n); // Necessita de ESTADO, RegrasInit
+int executarComando(char *buf, ESTADO *j, RegrasMovAuto rma, RegrasInit ri, RegrasBaralhos rb, RegrasWin rw, const char *nome); // Necessita de ESTADO, RegrasMovAuto, RegrasInit, RegrasBaralhos, RegrasWin
+void loopComandos(ESTADO *j, RegrasMovAuto rma, RegrasInit ri, RegrasBaralhos rb, RegrasWin rw, const char *nome); // Necessita de ESTADO, RegrasMovAuto, RegrasInit, RegrasBaralhos, RegrasWin
+void prepararAmbiente(ESTADO *j, RegrasBaralhos rb, RegrasMovAuto rma, RegrasWin rw); // Necessita de ESTADO, RegrasBaralhos, RegrasMovAuto, RegrasWin
+void execute(RegrasMovAuto rma, RegrasJogo rj, RegrasBaralhos rb, RegrasTipo rt, RegrasInit ri, RegrasWin rw, int carregar_save); // Necessita de todos os tipos de regras e ESTADO
--- Fase3/interface.h
+++
@@ -1,9 +0,0 @@
-#include "logica.h"
-#include "estado.h"
-
-char visibilidadeTipo(RegrasTipo rt, const char *tipo);
-void printCelula(ESTADO *j, int i, int lin);
-void imprimirTabuleiro(ESTADO *j);
-void mostrarEstado(ESTADO *j);
-void imprimirMovs(RegrasMovAuto rma);
-void imprimirWins(RegrasWin rw);
